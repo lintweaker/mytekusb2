9adecdaadcbfc83fb9360315360d079d921b47e6
Jurgen Kramer <gtmkramer@xs4all.nl>
2013-11-24 13:20:51 +0100

    Fix probe of multiple cards

--- chip.c
+++ chip.c
@@ -98,7 +98,7 @@ static int mytek_chip_probe(struct usb_interface *intf,
 			usb_set_intfdata(intf, chips[i]);
 			mutex_unlock(&register_mutex);
 			return 0;
-		} else if (regidx < 0)
+		} else if (!devices[i] && regidx < 0)
 			regidx = i;
 	}
 	if (regidx < 0) {
